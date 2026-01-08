9aed63d630aee1f7d55cfb95883b79423008675a
HemaReddy87 <hemahema11290@gmail.com>
2026-01-08 10:31:54 +0530

    Refactor timer and submitTest function scope

--- script.js
+++ script.js
@@ -1,28 +1,31 @@
-let timeLeft = 60;
+document.addEventListener("DOMContentLoaded", function () {
 
-let timer = setInterval(() => {
-  document.getElementById("timer").innerText =
-    "Time Left: " + timeLeft + " seconds";
+  let timeLeft = 60;
+  let timerElement = document.getElementById("timer");
 
-  timeLeft--;
+  let timer = setInterval(() => {
+    timerElement.innerText = "Time Left: " + timeLeft + " seconds";
+    timeLeft--;
 
-  if (timeLeft < 0) {
+    if (timeLeft < 0) {
+      clearInterval(timer);
+      submitTest();
+    }
+  }, 1000);
+
+  window.submitTest = function () {
     clearInterval(timer);
-    submitTest();
-  }
-}, 1000);
 
-function submitTest() {
-  clearInterval(timer);
+    let score = 0;
 
-  let score = 0;
+    if (document.querySelector('input[name="q1"]:checked')?.value === "30") score++;
+    if (document.querySelector('input[name="q2"]:checked')?.value === "JavaScript") score++;
+    if (document.querySelector('input[name="q3"]:checked')?.value === "HyperText Markup Language") score++;
+    if (document.querySelector('input[name="q4"]:checked')?.value === "Styling") score++;
+    if (document.querySelector('input[name="q5"]:checked')?.value === "Programming Language") score++;
 
-  if (document.querySelector('input[name="q1"]:checked')?.value === "30") score++;
-  if (document.querySelector('input[name="q2"]:checked')?.value === "JavaScript") score++;
-  if (document.querySelector('input[name="q3"]:checked')?.value === "HyperText Markup Language") score++;
-  if (document.querySelector('input[name="q4"]:checked')?.value === "Styling") score++;
-  if (document.querySelector('input[name="q5"]:checked')?.value === "Programming Language") score++;
+    document.getElementById("result").innerText =
+      "Your Score: " + score + " / 5";
+  };
 
-  document.getElementById("result").innerText =
-    "Your Score: " + score + " / 5";
-}
+});
