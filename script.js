0b6f3c3806de42daeb7f923d8ac8152098888123
HemaReddy87 <hemahema11290@gmail.com>
2026-01-08 10:25:23 +0530

    Refactor quiz timer and submission functions

--- script.js
+++ script.js
@@ -1,23 +1,19 @@
 let timeLeft = 60;
-let timer = document.getElementById("time");
 
-let countdown = setInterval(() => {
+let timer = setInterval(() => {
+  document.getElementById("timer").innerText =
+    "Time Left: " + timeLeft + " seconds";
+
   timeLeft--;
-  timer.textContent = timeLeft;
 
-  if (timeLeft <= 0) {
-    clearInterval(countdown);
-    submitQuiz();
+  if (timeLeft < 0) {
+    clearInterval(timer);
+    submitTest();
   }
 }, 1000);
 
-document.getElementById("quiz-form").addEventListener("submit", function (e) {
-  e.preventDefault();
-  submitQuiz();
-});
-
-function submitQuiz() {
-  clearInterval(countdown);
+function submitTest() {
+  clearInterval(timer);
 
   let score = 0;
 
@@ -28,5 +24,5 @@ function submitQuiz() {
   if (q2 && q2.value === "JavaScript") score++;
 
   document.getElementById("result").innerText =
-    "Your Score: " + score + "/2";
+    "Your Score: " + score + " / 2";
 }
